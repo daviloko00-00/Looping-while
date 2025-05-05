fae87c2889dfca2b44bfa95a4de002330bee0db9
unknown <davicustodiosilva36@gmail.com>
2025-05-05 15:42:29 -0300

    commit inicial

--- looping.js
+++ looping.js
@@ -1,7 +1,8 @@
 let n = 0;
 
 do {
-    let n = parseFloat(prompt("digite um número"));
-    if(isNaN(n)){
+    n = parseFloat(prompt("digite um número positivo"));
+    if(isNaN(n) || n <= 0){
         alert("não é um número válido");}
+    
 }while(n<0 || isNaN(n));
