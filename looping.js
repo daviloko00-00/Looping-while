b2333891e916e95968ccaa57ee5a116ffa3dcb7e
unknown <davicustodiosilva36@gmail.com>
2025-05-05 16:10:22 -0300

    Arrumado

--- looping.js
+++ looping.js
@@ -1,13 +1,15 @@
 let nota = 0;
 n= 0;
-do {n = n ++;
-    nota = parseFloat(prompt(`digite a nota ${i}` ));
+let soma = 0;
+do {
+    n++;
+    nota = parseFloat(prompt(`digite a nota ${n}` ));
     if (isNaN(nota) || nota <= 0) {
         alert("não é um número válido");
     }else {
         soma += nota
     }
 
-} while (nota > 0 || isNaN(nota))
+} while (nota >= 0 || isNaN(nota))
 media = soma/n;
-alert(`a média das notas é ${media}\n as notas colocadas foram ${nota}`)
+alert(`a média das notas é ${media}`)
