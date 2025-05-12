00e368238cafc95287046b62dbed2dc5ec59e83f
unknown <davicustodiosilva36@gmail.com>
2025-05-12 12:41:56 -0300

    Código inicial

--- looping.js
+++ looping.js
@@ -1,15 +1,19 @@
-let produto = 0;
+let nome = "";
 n= 0;
-let soma = 0;
+let sobrenome = "";
 do {
-    n++;
-    produto = parseFloat(prompt(`digite o valor do produto ${n}:` ));
-    if (isNaN(produto) || produto < 0) {
-        alert("não é um número válido");
-    }else {
-        soma += produto
+    if (n = 0){
+        nome = prompt("Diga seu nome:")
+        if (nome != String){
+            alert("Digite um nome e não um número")
+        } else if(n = 1){
+            nome = prompt("Diga seu nome:")
+        if (nome != String){
+            alert("Digite um nome e não um número")
+       
     }
-
+    n++;
+        }
 } while (produto != 0 || isNaN(produto) || produto > 0)
 
 alert(`O total das compras foi de ${soma}`)
