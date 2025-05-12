9a0f749e9df2b862e3cb7dab89ef419e3a108194
unknown <davicustodiosilva36@gmail.com>
2025-05-12 12:52:18 -0300

    hmtl resolvido

--- looping.js
+++ looping.js
@@ -3,6 +3,6 @@ let n = 0;
 do {
     n = parseFloat(prompt("digite um número positivo"));
     if(isNaN(n) || n <= 0){
-        alert("não é um número válido");}
+        alert("não é um número válido, por favor tente novamente");}
     
 }while(n<0 || isNaN(n));
