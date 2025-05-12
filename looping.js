227f674b0e6674023e8e651c7647190a777cc6d5
unknown <davicustodiosilva36@gmail.com>
2025-05-12 13:26:50 -0300

    código comentado

--- looping.js
+++ looping.js
@@ -1,15 +1,16 @@
-let nota = 0;
-n= 0;
-let soma = 0;
+let nota = 0;// variável para declarar notas que serão colocadas. 
+n= 0;// variável que conta quantas notas já foram adicionadas.
+let soma = 0;// variável que faz a soma das notas já colocadas.
 do {
-    n++;
+    n++;//adição para sempre estar atualizado em relação à nota
     nota = parseFloat(prompt(`digite a nota ${n}` ));
-    if (isNaN(nota) || nota <= 0) {
+    if (isNaN(nota) || nota <= 0 || nota >= 10) {
         alert("não é um número válido");
     }else {
-        soma += nota
+        soma += nota // atribui e adiciona valores a soma
     }
 
 } while (nota >= 0 || isNaN(nota))
-media = (soma/n).toFixed(2);
-alert(`a média das notas é ${media}`)
+media = (soma/n).toFixed(2);// faz a média dos valores pelo número de notas
+alert(`a média das notas é ${media}`)//saída final do código.
+alert (`as notas colocadas foram ${nota}`)
