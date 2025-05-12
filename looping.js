144e48d8636f3dc9eb986fcc8646a367ad3021cb
unknown <davicustodiosilva36@gmail.com>
2025-05-12 13:41:17 -0300

    código comentado

--- looping.js
+++ looping.js
@@ -1,15 +1,15 @@
-let produto = 0;
-n= 0;
-let soma = 0;
+let produto = 0;// variável que recebe o preço dos produtos.
+n= 0;// variável que onta quantos valores já foram adicionados.
+let soma = 0;// variável que faz a soma dos valores já dados
 do {
     n++;
     produto = parseFloat(prompt(`digite o valor do produto ${n}:` ));
     if (isNaN(produto) || produto < 0) {
         alert("não é um número válido");
     }else {
-        soma += produto
+        soma += produto// adiciona e atribui o valor dos produtos à soma.
     }
 
-} while (produto != 0 || isNaN(produto) || produto > 0)
+} while (produto != 0 || isNaN(produto) || produto > 0)// estrutura lógica em relação ao looping 
 
-alert(`O total das compras foi de ${soma}`)
+alert(`O total das compras foi de ${soma}`)// sáida do código e valores
