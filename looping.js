63c7736da485c57b2fbcea75152dc1719c109267
unknown <davicustodiosilva36@gmail.com>
2025-05-12 13:20:14 -0300

    código comentado

--- looping.js
+++ looping.js
@@ -1,8 +1,8 @@
-let n = 0;
+let n = 0;// variável para o número que será analisado durante o looping
 
-do {
+do {// inserção de valores para n, de forma a observar se n é um número positivo ou não
     n = parseFloat(prompt("digite um número positivo"));
     if(isNaN(n) || n <= 0){
         alert("não é um número válido, por favor tente novamente");}
     
-}while(n<0 || isNaN(n));
+}while(n<0 || isNaN(n));// estrutura lógica para o looping caso os valores sejam inválidos
