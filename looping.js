33f876b84880acc1fd97e19f045a650b112af2f6
unknown <davicustodiosilva36@gmail.com>
2025-05-12 13:11:19 -0300

    código resolvido de vdd kkkkk

--- looping.js
+++ looping.js
@@ -2,14 +2,14 @@ let nome = "";
 n = 0;
 let sobrenome = "";
 do {
-    if (n = 0) {
+    
         nome = prompt("Diga seu nome:");
         sobrenome = prompt("Diga seu sobrenome:");
         if(!(isNaN(nome && sobrenome)) || nome === "" || sobrenome === "" ){
             alert("digite um nome e sobrenome válido");
         }  
             n++;
-        }
+        
     }
  while (!isNaN(nome&&sobrenome) || sobrenome === "" || nome === "" );
 
